9ef0aca4974374490e85e97b252901d4a6100c57
Komplexnupe <leonard.love87@icloud.com>
2020-08-06 23:08:20 -0400

    Initial

---
+++ nfl-quiz.js
@@ -0,0 +1,107 @@
+var startButtonEl = document.getElementById("start-btn");
+var timerEl = document.getElementById("timer");
+var questionEl = document.getElementById('quiz-questions');
+var answerButtonEl = document.getElementById('answer-buttons');
+
+var timerInterval;
+var secondsLeft = 75;
+var timeCap = 0;
+let currentQuestion = [];
+
+var questions = [
+  {
+    question: "Who is the best QB in the NFL?",
+    choices: [
+      { text: "Lamar Jackson", correct: false },
+      { text: "Russell Wilson", correct: true },
+      { text: "Pat Mahomes", correct: false },
+      { text: "Aaron Rodgers", correct: false },
+      { text: "Tom Brady", correct: false },
+    ],
+
+  },
+  {
+    question: "Who is the greatest QB of all time?",
+    choices: [
+      { text: "Dan Marino", correct: false },
+      { text: "Peyton Manning", correct: false },
+      { text: "Tom Brady", correct: true },
+    ],
+
+  },
+  {
+    question: " Who is the greatest LB of all time?",
+    choices: [
+      { text: "Dick Butkus", correct: false },
+      { text: "Ray Lewis", correct: true },
+      { text: "Brian Urlacher", correct: false },
+      { text: "Luke Keuchly", correct: false },
+      { text: "Lawerence Taylor", correct: false },
+    ],
+
+  },
+  {
+    question: "Who is the greatest safety of all time?",
+    choices: [
+      { text: "Troy Polamlu", correct: false },
+      { text: "Sean Taylor", correct: false },
+      { text: "Ed Reed", correct: true },
+      { text: "Ronnie Lott", correct: false },
+    ],
+
+  },
+  {
+    question: 'Who is the best WR currently in the NFL?',
+    choices: [
+      { text: "Odell Beckham", correct: false },
+      { text: "Julio Jones", correct: false },
+      { text: "Deandre Hopkins", correct: true },
+      { text: "Michael Thomas", correct: false },
+
+    ]
+  }
+
+];
+
+
+// Begin Quiz
+
+startButtonEl.addEventListener('click', startQuiz);
+
+function startQuiz() {
+  console.log("Started");
+  startButtonEl.parentNode.parentNode.classList.add("d-none");
+  questionEl.parentNode.classList.remove("d-none");
+  currentQuestion.questions[0].question
+  console.log(currentQuestion);
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft;
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+
+  }, 1000);
+
+  nextQuestion()
+  
+}
+
+function nextQuestion() {
+
+}
+
+function showQuestions(question) {
+  questionEl.innerHTML = question.question
+
+}
+
+
+function selectAnswer() {
+
+}
+
+function endQuiz() {
+
+};
---
+++ psuedo.md
@@ -0,0 +1,52 @@
+#Coding Quiz Steps
+
+*Show User "Football Quiz Challenge"
+    *have timer at the start in upper right hand corner start at 75 seconds & decrement
+    *view highscores link in upper left
+    *provide start button to start quiz
+
+*Quiz flow
+    *user is given 4 choices
+    *if user selects correct answer, do nothing and change button color to green
+    *if user selects incorrect answer, deduct 15 seconds and change button color to red
+    *after answer remove/hide content and show new content
+    *redirect to high scores page after last quesiton
+
+
+*High Scores Screen
+    *When user completes quiz, push remaining time to array or object
+    *order list from highest to lowest number
+
+
+*Questions:
+    Who is the best QB in the NFL?
+    - Lamar Jackson
+    - Pat Mahomes
+    - Aaron Rodgers
+    - Russell Wilson !
+    - Tom Brady
+
+    Who is the best WR in the NFL?
+    - Odell Beckham
+    - Julio Jones
+    - Deandre Hopkins !
+    - Michael Thomas
+
+    Who is the greatest QB of all time?
+    - Dan Marino
+    - Peyton Manning !
+    - Tom Brady 
+    - Brett Favre
+
+    Who is the greatest safety of all time?
+    - Ed Reed !
+    - Troy Polamlu
+    - Ronnie Lott
+    - Sean Taylor
+
+    Who is the greatest LB of all time?
+    - Dick Butkus
+    - Ray Lewis !
+    - Brian Urlacher
+    - Luke Keuchly
+    - Lawerence Taylor
